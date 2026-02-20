f77022e0633a9de8a213f369097ceeaf6c1d22bd
MaxwellM.C <maxwellbom93@gmail.com>
2026-02-20 20:27:22 +0000

    CRIEI O CODIGO, FIZ PASTAS COM O NOME DE CADA ESTADO, E DENTRO DE CADA PASTA CRIEI 4 CARTAS PARA CADA ESTADO, O CODIGO MOSTRA OS DADOS DE CADA CADASTRO.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,12 +4,13 @@
 // Tema 1 - Cadastro das cartas
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
+
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
-
+  
 return 0;
 } 
