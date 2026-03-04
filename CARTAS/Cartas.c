86f2a9a8cc2f9e387352cf171cdadb8941718be0
MaxwellM.C <maxwellbom93@gmail.com>
2026-03-04 20:08:29 +0000

    ATUALIZEI O CODIGO ADICIONEI A COMPARAÇÃO DE TODAS AS VARIÁVEIS, INCLUSIVE O SUPER PODER, PARA QUE A BATALHA DAS CARTAS FIQUE MAIS INTERESSANTE. ALÉM DISSO, CORRIGI ALGUNS ERROS DE SINTAXE E MELHOREI A EXIBIÇÃO DOS RESULTADOS DA BATALHA.

--- CARTAS/Cartas.c
+++ CARTAS/Cartas.c
@@ -14,14 +14,16 @@ int main() {
   char Estado [20]; // Estado
   char codigoCidade [20]; // Código da cidade
   char cidade [20]; // Nome da cidade
+  char resultado01 = 'A';
 
-  int populacao; // Quantidade da populacao na cidade
+  unsigned int populacao; // Quantidade da populacao na cidade
   int Turisticos; // Numero de pontos turisticos!
 
   float area;
   float PIB;
   float densidade; // densidade populacional (populacao / area);
   float perCapita; // PIB per capita (PIB / populacao);
+  float superPoder01; // Super poder 1
 
   // VARIAVEIS PARA A SEGUNDA CARTA
 
@@ -29,14 +31,16 @@ int main() {
   char Estado2 [20]; // Estado
   char codigoCidade2 [20]; // Código da cidade
   char cidade2 [20]; // Nome da cidade
+  char resultado02 = 'B';
 
-  int populacao2; // Quantidade da populacao na cidade
+  unsigned int populacao2; // Quantidade da populacao na cidade
   int Turisticos2; // Numero de pontos turisticos!
 
   float area2;
   float PIB2;
   float densidade2; // densidade populacional (populacao / area);
   float perCapita2; // PIB per capita (PIB / populacao);
+  float superPoder02; // Super poder 2
 
   // Área para entrada de dados
   // AQUI IRÁ INSERIR DADOS!
@@ -76,8 +80,11 @@ int main() {
   printf("Qual o PIB da cidade: \n");
   scanf("%f", &PIB);
 
+// ---------------------- OPERADORES CARTA 01 ---------------------- 
+
   densidade = populacao / area;
-  perCapita = PIB / populacao;  
+  perCapita = PIB / populacao; 
+  superPoder01 = populacao + Turisticos + area + PIB + perCapita + (1.0 / densidade);
 
   // -------------------- CADASTRO DA SEGUNDA CARTA --------------------
 
@@ -111,11 +118,14 @@ int main() {
   printf("Qual o PIB da cidade: \n");
   scanf("%f", &PIB2);
 
+  // ---------------------- OPERADORES CARTA 02 ---------------------- 
+
   densidade2 = populacao2 / area2;
   perCapita2 = PIB2 / populacao2;
+  superPoder02 = populacao2 + Turisticos2 + area2 + PIB2 + perCapita2 + (1.0 / densidade2);
 
-  // Área para exibição dos dados da cidade
 
+  // Área para exibição dos dados da cidadE
 
   // ---------------------- DADOS DA PRIMEIRA CARTA ---------------------- 
   printf("----- DADOS CADASTRADOS ----- \n");
@@ -142,6 +152,19 @@ int main() {
   printf("PIB: %.2f\n", PIB2);
   printf("densidade Populacional: %.2f\n", densidade2);
   printf("PIB per capita: %.2f\n", perCapita2);
-  
+
+
+// ---------------------- BATALHA DAS CARTAS ----------------------
+
+printf("----- BATALHA DAS CARTAS ----- \n");
+printf("----- VENCE A CARTA QUE TIVER O VALOR MAIOR ----- \n");
+printf("----- CARTA 1 (1 NUMERO DA CARTA) | CARTA 2 (0 NUMERO DA CARTA)  ----- \n");
+printf("POPULACAO: %d\n", populacao > populacao2);
+printf("QUANTIDADE DE PONTOS TURISTICOS: %d\n", Turisticos > Turisticos2);
+printf("AREA EM KMS: %d\n", area > area2);
+printf("PIB: %d\n", PIB > PIB2);
+printf("DENSIDADE POPULACIONAL: %d\n", (1.0 - densidade) < (1.0 - densidade2));
+printf("PIB PER CAPITA: %d\n", perCapita > perCapita2);
+printf("SUPER PODER: %d", resultado01 > resultado02);
 return 0;
 } 
